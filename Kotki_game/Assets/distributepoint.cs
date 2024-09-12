e99fd642defe7e9d061eaf4b5f82d2a836a8ba13
Bomby888 <115494214+Bomby888@users.noreply.github.com>
2024-09-12 09:29:40 +0200

    zmiana distributepoints
    distributepoints teraz korzysta z klasy VertexPoint

--- Kotki_game/Assets/distributepoint.cs
+++ Kotki_game/Assets/distributepoint.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class distributeverticies : MonoBehaviour
 {
     [SerializeField]
@@ -13,14 +14,15 @@ public class distributeverticies : MonoBehaviour
     [SerializeField]
     float gizmosRadius = 0.1f;
 
-    List<Vector3> verticies;
+    List<VertexPoint> verticies;
 
     List<Face> faces;
 
     private void Start()
     {
-        verticies = new List<Vector3>();
+        verticies = new List<VertexPoint>();
         faces = new List<Face>();
+        Debug.Log("skibidi toilett");
 
         IcoSphereDistribution();
         for (int i = 1; i < numSubdivisions; i++)
@@ -30,68 +32,67 @@ public class distributeverticies : MonoBehaviour
         PlaceAllVertsOnSphere();
         ScaleAndMove();
 
-        Debug.Log(verticies.Count);
+        List<Vector3> positions = GetVertexPositions();
+        Debug.Log("Liczba pozycji: " + positions.Count);
     }
+
     void IcoSphereDistribution()
     {
-        //basic d20
-        //top
         Vector3 heightShift = new Vector3(0, ((0.5f + Mathf.Sqrt(5) / 10.0f)-(0.5f * (1.0f - 1.0f / Mathf.Sqrt(5))))/2, 0);
         float a = 0;
-        for (int i = 0; i < 5; i++) {
-            verticies.Add(1.0f/ Mathf.Sqrt(5) * new Vector3(Mathf.Sin(a),0,Mathf.Cos(a))+heightShift);
+        for (int i = 0; i < 5; i++) 
+        {
+            verticies.Add(new VertexPoint(1.0f/ Mathf.Sqrt(5) * new Vector3(Mathf.Sin(a),0,Mathf.Cos(a))+heightShift));
             a += Mathf.PI * 2 / 5;
         }
-        verticies.Add(new Vector3(0, 0.5f * (1.0f - 1.0f / Mathf.Sqrt(5)), 0)+heightShift);
+        verticies.Add(new VertexPoint(new Vector3(0, 0.5f * (1.0f - 1.0f / Mathf.Sqrt(5)), 0)+heightShift));
 
-        faces.Add(new Face(verticies[0], verticies[1], verticies[5]));
-        faces.Add(new Face(verticies[1], verticies[2], verticies[5]));
-        faces.Add(new Face(verticies[2], verticies[3], verticies[5]));
-        faces.Add(new Face(verticies[3], verticies[4], verticies[5]));
-        faces.Add(new Face(verticies[4], verticies[0], verticies[5]));
+        faces.Add(new Face(verticies[0].position, verticies[1].position, verticies[5].position));
+        faces.Add(new Face(verticies[1].position, verticies[2].position, verticies[5].position));
+        faces.Add(new Face(verticies[2].position, verticies[3].position, verticies[5].position));
+        faces.Add(new Face(verticies[3].position, verticies[4].position, verticies[5].position));
+        faces.Add(new Face(verticies[4].position, verticies[0].position, verticies[5].position));
 
-        //bottom
         heightShift = -heightShift;
-        a = Mathf.PI*2/10;
+        a = Mathf.PI * 2 / 10;
         for (int i = 0; i < 5; i++)
         {
-            verticies.Add(1.0f / Mathf.Sqrt(5) * new Vector3(Mathf.Sin(a), 0, Mathf.Cos(a)) + heightShift);
+            verticies.Add(new VertexPoint(1.0f / Mathf.Sqrt(5) * new Vector3(Mathf.Sin(a), 0, Mathf.Cos(a)) + heightShift));
             a += Mathf.PI * 2 / 5;
         }
-        verticies.Add(new Vector3(0, -0.5f * (1.0f - 1.0f / Mathf.Sqrt(5)), 0) + heightShift);
+        verticies.Add(new VertexPoint(new Vector3(0, -0.5f * (1.0f - 1.0f / Mathf.Sqrt(5)), 0) + heightShift));
 
-        faces.Add(new Face(verticies[6], verticies[7], verticies[11]));
-        faces.Add(new Face(verticies[7], verticies[8], verticies[11]));
-        faces.Add(new Face(verticies[8], verticies[9], verticies[11]));
-        faces.Add(new Face(verticies[9], verticies[10], verticies[11]));
-        faces.Add(new Face(verticies[10], verticies[6], verticies[11]));
+        faces.Add(new Face(verticies[6].position, verticies[7].position, verticies[11].position));
+        faces.Add(new Face(verticies[7].position, verticies[8].position, verticies[11].position));
+        faces.Add(new Face(verticies[8].position, verticies[9].position, verticies[11].position));
+        faces.Add(new Face(verticies[9].position, verticies[10].position, verticies[11].position));
+        faces.Add(new Face(verticies[10].position, verticies[6].position, verticies[11].position));
 
-        //middle
-        faces.Add(new Face(verticies[0], verticies[10], verticies[6]));
-        faces.Add(new Face(verticies[1], verticies[6], verticies[7]));
-        faces.Add(new Face(verticies[2], verticies[7], verticies[8]));
-        faces.Add(new Face(verticies[3], verticies[8], verticies[9]));
-        faces.Add(new Face(verticies[4], verticies[9], verticies[10]));
+        faces.Add(new Face(verticies[0].position, verticies[10].position, verticies[6].position));
+        faces.Add(new Face(verticies[1].position, verticies[6].position, verticies[7].position));
+        faces.Add(new Face(verticies[2].position, verticies[7].position, verticies[8].position));
+        faces.Add(new Face(verticies[3].position, verticies[8].position, verticies[9].position));
+        faces.Add(new Face(verticies[4].position, verticies[9].position, verticies[10].position));
 
-        faces.Add(new Face(verticies[6], verticies[0], verticies[1]));
-        faces.Add(new Face(verticies[7], verticies[1], verticies[2]));
-        faces.Add(new Face(verticies[8], verticies[2], verticies[3]));
-        faces.Add(new Face(verticies[9], verticies[3], verticies[4]));
-        faces.Add(new Face(verticies[10], verticies[4], verticies[0]));
+        faces.Add(new Face(verticies[6].position, verticies[0].position, verticies[1].position));
+        faces.Add(new Face(verticies[7].position, verticies[1].position, verticies[2].position));
+        faces.Add(new Face(verticies[8].position, verticies[2].position, verticies[3].position));
+        faces.Add(new Face(verticies[9].position, verticies[3].position, verticies[4].position));
+        faces.Add(new Face(verticies[10].position, verticies[4].position, verticies[0].position));
     }
 
     void ScaleAndMove()
     {
-        for(int i = 0; i < verticies.Count; i++)
+        for (int i = 0; i < verticies.Count; i++)
         {
-            verticies[i] = (verticies[i] * scale/2) + transform.position;
+            verticies[i].position = (verticies[i].position * scale / 2) + transform.position;
         }
 
         for (int i = 0; i < faces.Count; i++)
         {
-            faces[i].vertA = faces[i].vertA * scale/2 + transform.position;
-            faces[i].vertB = faces[i].vertB * scale/2 + transform.position;
-            faces[i].vertC = faces[i].vertC * scale/2 + transform.position;
+            faces[i].vertA = faces[i].vertA * scale / 2 + transform.position;
+            faces[i].vertB = faces[i].vertB * scale / 2 + transform.position;
+            faces[i].vertC = faces[i].vertC * scale / 2 + transform.position;
         }
     }
 
@@ -99,7 +100,7 @@ public class distributeverticies : MonoBehaviour
     {
         for (int i = 0; i < verticies.Count; i++)
         {
-            verticies[i] = verticies[i].normalized;
+            verticies[i].position = verticies[i].position.normalized;
         }
 
         for (int i = 0; i < faces.Count; i++)
@@ -113,25 +114,25 @@ public class distributeverticies : MonoBehaviour
     void Subdivide()
     {
         List<Face> newFaces = new List<Face>();
-        List<Vector3> newVerts = new List<Vector3>();
+        List<VertexPoint> newVerts = new List<VertexPoint>();
 
-        foreach(var face in faces)
+        foreach (var face in faces)
         {
             Vector3 midPointAB = midPoint(face.vertA, face.vertB);
             Vector3 midPointBC = midPoint(face.vertB, face.vertC);
             Vector3 midPointCA = midPoint(face.vertC, face.vertA);
 
-            newVerts.Add(face.vertA);
-            newVerts.Add(face.vertB);
-            newVerts.Add(face.vertC);
-            newVerts.Add(midPointAB);
-            newVerts.Add(midPointBC);
-            newVerts.Add(midPointCA);
+            newVerts.Add(new VertexPoint(face.vertA));
+            newVerts.Add(new VertexPoint(face.vertB));
+            newVerts.Add(new VertexPoint(face.vertC));
+            newVerts.Add(new VertexPoint(midPointAB));
+            newVerts.Add(new VertexPoint(midPointBC));
+            newVerts.Add(new VertexPoint(midPointCA));
 
-            newFaces.Add(new Face(face.vertA,midPointCA,midPointAB));
-            newFaces.Add(new Face(face.vertB,midPointBC,midPointAB));
-            newFaces.Add(new Face(face.vertC,midPointBC,midPointCA));
-            newFaces.Add(new Face(midPointAB,midPointBC,midPointCA));
+            newFaces.Add(new Face(face.vertA, midPointCA, midPointAB));
+            newFaces.Add(new Face(face.vertB, midPointBC, midPointAB));
+            newFaces.Add(new Face(face.vertC, midPointBC, midPointCA));
+            newFaces.Add(new Face(midPointAB, midPointBC, midPointCA));
         }
 
         verticies = newVerts;
@@ -143,39 +144,39 @@ public class distributeverticies : MonoBehaviour
     {
         HashSet<Vector3> hashVerts = new HashSet<Vector3>();
 
-        foreach(var vert in verticies)
+        foreach (var vert in verticies)
         {
-            hashVerts.Add(vert);
+            hashVerts.Add(vert.position);
         }
 
         verticies.Clear();
 
-        foreach(var vert in hashVerts)
+        foreach (var vert in hashVerts)
         {
-            verticies.Add(vert);
+            verticies.Add(new VertexPoint(vert));
         }
     }
+
     Vector3 midPoint(Vector3 v1, Vector3 v2)
     {
         return new Vector3((v1.x + v2.x) / 2, (v1.y + v2.y) / 2, (v1.z + v2.z) / 2);
     }
-
 private void OnDrawGizmos()
 {
     Gizmos.color = Color.red;
 
-    // Sprawdź, czy lista verticies została zainicjalizowana
+
     if (verticies != null)
     {
         foreach (var pt in verticies)
         {
-            Gizmos.DrawSphere(pt, gizmosRadius); // Rysujemy sfery bez dodatkowego skalowania
+            Gizmos.DrawSphere(pt.position, gizmosRadius); 
         }
     }
 
     Gizmos.color = Color.yellow;
 
-    // Sprawdź, czy lista faces została zainicjalizowana
+
     if (faces != null)
     {
         foreach (var face in faces)
@@ -186,4 +187,16 @@ private void OnDrawGizmos()
         }
     }
 }
+public List<Vector3> GetVertexPositions()
+{
+    List<Vector3> positions = new List<Vector3>();
+
+    foreach (var vertexPoint in verticies)
+    {
+        positions.Add(vertexPoint.position);
+    }
+
+    return positions;
+}
+
 }
