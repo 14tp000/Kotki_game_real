d17d6605a2ecd543ae7f7508de6e9c96bf94cfde
Bomby888 <115494214+Bomby888@users.noreply.github.com>
2024-09-12 09:10:52 +0200

    dodanie klasy vertexpoint

---
+++ Kotki_game/Assets/vertexpoint.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexPoint
+{
+    public Vector3 position;
+    public VertexPoint(Vector3 position)
+    {
+        this.position = position;
+
+    }
+}
