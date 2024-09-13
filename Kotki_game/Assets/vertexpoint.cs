90727b60f6cf2474c6e4f98c7f6b8283ee002a32
Bomby888 <115494214+Bomby888@users.noreply.github.com>
2024-09-13 09:31:35 +0200

    dodanie metody setneighbours
    prosta logika na dodawanie sasiadow wewnatrz klasy vertexpoint

--- Kotki_game/Assets/vertexpoint.cs
+++ Kotki_game/Assets/vertexpoint.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class VertexPoint
 {
     public Vector3 position;
+      public Vector3[] neighbours = new Vector3[6];
     public VertexPoint(Vector3 position)
     {
         this.position = position;
 
     }
+    public void SetNeighbour(Vector3 neighbour)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (neighbours[i] == neighbour)
+            {
+                break;
+            }
+
+            if (neighbours[i] == Vector3.zero)
+            {
+                neighbours[i] = neighbour;
+                break;
+            }
+        }
+}
+
+
 }
