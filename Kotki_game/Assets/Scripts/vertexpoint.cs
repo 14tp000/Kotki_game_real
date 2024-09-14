df959c0ee5164c598b39e33e75acf59842cc8047
14tp00 <118660337+14tp000@users.noreply.github.com>
2024-09-14 16:55:23 +0200

    czystszy kod

--- Kotki_game/Assets/Scripts/vertexpoint.cs
+++ Kotki_game/Assets/Scripts/vertexpoint.cs
@@ -7,27 +7,8 @@ public class VertexPoint
     public Vector3 position;
     public List<VertexPoint> neighbours = new List<VertexPoint>();
     public bool highlighted;
-    int iterator = 0;
     public VertexPoint(Vector3 position)
     {
         this.position = position;
-    }
-    public void SetNeighbour(VertexPoint neighbour)
-    {
-        neighbours.Add(neighbour);
-        
-        //for (int i = 0; i < 6; i++)
-        //{
-        //    //if (neighbours[i] == neighbour)
-        //    //{
-        //    //    break;
-        //    //}
-        //    neighbours[i] = neighbour;
-        //    //if (neighbours[i].position == Vector3.zero)
-        //    //{
-        //    //    neighbours[i] = neighbour;
-        //    //    break;
-        //    //}
-        //}
     }
 }
